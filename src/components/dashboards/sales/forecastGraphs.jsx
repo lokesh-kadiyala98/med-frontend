2f1ed77ccdb3bb42a8b27ab856d0040e5ea9e31e
lokesh-kadiyala98 <lokesh.pandu1998@gmail.com>
2020-03-20 12:54:10 +0530

    Added Feature: Error check for medicines

--- src/components/dashboards/sales/forecastGraphs.jsx
+++ src/components/dashboards/sales/forecastGraphs.jsx
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
 import { Line } from 'react-chartjs-2';
 import axios from 'axios'
+import _ from 'lodash'
 
 import config from '../../../config.json'
-import HoltWinters from './holtWinters';
-import Loading from '../../misc/loading';
+import HoltWinters from './holtWinters'
+import Loading from '../../misc/loading'
 
 class ForecastGraphs extends Component {
     state = { 
-        medicineNames: [],
+        medicineNames: ['a','b','c'],
         loading: true,
         lineData: {
             labels: [],
@@ -20,106 +21,102 @@ class ForecastGraphs extends Component {
 
     async componentDidUpdate(prevProps) {
         const { medicineName } = this.props
-        
         if(prevProps.medicineName !== medicineName) {
             var medicineNames = [...this.state.medicineNames]
-            medicineNames.push(medicineName)
-            this.setState({ medicineNames, loading: true })
-            
-            const { data } = await axios.get(config.apiEndpoint + '/pharma/get_timeseries_data', {
-                params: {
-                    medicineName: medicineName
+            if(medicineNames.indexOf(medicineName) === -1) {
+                if(medicineNames.length < 4) {
+                    medicineNames.push(medicineName)
+                    this.setState({ medicineNames, loading: true })
+                    
+                    try {
+                        const { data } = await axios.get(config.apiEndpoint + '/pharma/get_timeseries_data', {
+                            params: {
+                                medicineName: medicineName
+                            }
+                        })
+                        var dataset = []
+                        const months = {"01": "Jan", "02": "Feb", "03": "Mar", "04": "Apr", "05": "May", "06": "June", "07": "July", "08": "Aug", "09": "Sept", "10": "Oct", "11": "Nov", "12": "Dec"}
+                        const colors = [{faded: 'rgba(253, 121, 168, 0.2)', unfaded: 'rgba(253, 121, 168, 1.0)'},{faded: 'rgba(253, 203, 110, 0.2)', unfaded: 'rgba(253, 203, 110, 1.0)'},{faded: 'rgba(9, 132, 227, 0.2)', unfaded: 'rgba(9, 132, 227, 1.0)'},{faded: 'rgba(108, 92, 231, 0.2)', unfaded: 'rgba(108, 92, 231, 1.0)'}]
+                        var lineData = {...this.state.lineData}
+                        
+                        lineData.labels = []
+                        data.forEach((item, key) => {
+                            lineData.labels.push(months[item.month] + '-' + item.year.substr(2, 4))
+                            dataset[key] = item.count
+                        })
+    
+                        lineData.labels.push("Jan-18")
+                        lineData.labels.push("Feb-18")
+                        lineData.labels.push("Mar-18")
+                        lineData.labels.push("Apr-18")
+                        
+                        const holtWintersResult = HoltWinters(dataset, this.state.predictionLength)
+                        var temp = {
+                            label: medicineName,
+                            fill: false,
+                            lineTension: 0.1,
+                            backgroundColor: colors[this.state.medicineNames.length - 1].faded,
+                            borderColor: colors[this.state.medicineNames.length - 1].unfaded,
+                            borderCapStyle: 'butt',
+                            borderJoinStyle: 'miter',
+                            pointBorderColor: colors[this.state.medicineNames.length - 1].unfaded,
+                            pointBackgroundColor: '#fff',
+                            pointBorderWidth: 1,
+                            pointHoverRadius: 5,
+                            pointHoverBackgroundColor: colors[this.state.medicineNames.length - 1].unfaded,
+                            pointHoverBorderColor: 'rgba(220, 220, 220, 1)',
+                            pointHoverBorderWidth: 2,
+                            pointRadius: 1,
+                            pointHitRadius: 10,
+                            data: holtWintersResult.augumentedDataset
+                        }
+                        lineData.datasets.push(temp)
+                        
+                        this.setState({ lineData, loading: false })
+                    } catch(ex) {
+                        console.log(ex)
+                    }
+
+                } else {
+                    var errors = {...this.state.errors}
+                    errors.message = 'Cannot compare more than 4 medicines'
+                    this.setState({ errors, loading: false })
                 }
-            })
-
-            var dataset = []
-            const months = {"01": "Jan", "02": "Feb", "03": "Mar", "04": "Apr", "05": "May", "06": "June", "07": "July", "08": "Aug", "09": "Sept", "10": "Oct", "11": "Nov", "12": "Dec"}
-            const colors = [{faded: 'rgba(253, 121, 168, 0.2)', unfaded: 'rgba(253, 121, 168, 1.0)'},{faded: 'rgba(253, 203, 110, 0.2)', unfaded: 'rgba(253, 203, 110, 1.0)'},{faded: 'rgba(9, 132, 227, 0.2)', unfaded: 'rgba(9, 132, 227, 1.0)'},{faded: 'rgba(108, 92, 231, 0.2)', unfaded: 'rgba(108, 92, 231, 1.0)'}]
-            var lineData = {...this.state.lineData}
-            
-            lineData.labels = []
-            data.forEach((item, key) => {
-                lineData.labels.push(months[item.month] + '-' + item.year.substr(2, 4))
-                dataset[key] = item.count
-            })
-
-            lineData.labels.push("Jan-18")
-            lineData.labels.push("Feb-18")
-            lineData.labels.push("Mar-18")
-            lineData.labels.push("Apr-18")
-            
-            const holtWintersResult = HoltWinters(dataset, this.state.predictionLength)
-            var temp = {
-                label: medicineName,
-                fill: false,
-                lineTension: 0.1,
-                backgroundColor: colors[this.state.medicineNames.length - 1].faded,
-                borderColor: colors[this.state.medicineNames.length - 1].unfaded,
-                borderCapStyle: 'butt',
-                borderJoinStyle: 'miter',
-                pointBorderColor: colors[this.state.medicineNames.length - 1].unfaded,
-                pointBackgroundColor: '#fff',
-                pointBorderWidth: 1,
-                pointHoverRadius: 5,
-                pointHoverBackgroundColor: colors[this.state.medicineNames.length - 1].unfaded,
-                pointHoverBorderColor: 'rgba(220, 220, 220, 1)',
-                pointHoverBorderWidth: 2,
-                pointRadius: 1,
-                pointHitRadius: 10,
-                data: holtWintersResult.augumentedDataset
+            } else {
+                var errors = {...this.state.errors}
+                errors.message = medicineName + ' already given'
+                this.setState({ errors, loading: false })
             }
-            lineData.datasets.push(temp)
-            
-            this.setState({ lineData, loading: false })
         }
     }
 
     handleRemoveMedicine = (index) => {
         var medicineNames = [...this.state.medicineNames]
         var lineData = {...this.state.lineData}
+        var errors = {...this.state.errors}
+        var loading = false
         medicineNames.splice(index, 1)
         lineData.datasets.splice(index, 1)
-        this.setState({ lineData, medicineNames })
+        delete errors.message
+        if(medicineNames.length === 0)
+            loading = true
+        this.setState({ lineData, medicineNames, errors, loading })
     }
     
-    render() { 
-        // const data = {
-        //     labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-        //     datasets: [
-        //       {
-        //         label: 'My First dataset',
-        //         fill: false,
-        //         lineTension: 0.1,
-        //         borderColor: 'rgba(75,192,192,1)',
-        //         borderCapStyle: 'butt',
-        //         borderDash: [],
-        //         borderDashOffset: 0.0,
-        //         borderJoinStyle: 'miter',
-        //         pointBorderColor: 'rgba(75,192,192,1)',
-        //         pointBackgroundColor: '#fff',
-        //         pointBorderWidth: 1,
-        //         pointHoverRadius: 5,
-        //         pointHoverBackgroundColor: 'rgba(75,192,192,1)',
-        //         pointHoverBorderColor: 'rgba(220,220,220,1)',
-        //         pointHoverBorderWidth: 2,
-        //         pointRadius: 1,
-        //         pointHitRadius: 100,
-        //         data: [65, 59, 80, 81, 56, 55, 40]
-        //       }
-        //     ]
-        // }
-        const { lineData, medicineNames, loading } = this.state
+    render() {
+        const { lineData, medicineNames, loading, errors } = this.state
 
         if(loading)
             return <Loading />
-
+        
         return ( 
             <React.Fragment>
+                {errors.message && <div className="alert alert-warning">{errors.message}</div>}
                 <Line data={lineData} />
                 <div className="mt-5 capsules">
                     {medicineNames.map((item, index) => 
                         <li className="delete" key={index} onClick={() => this.handleRemoveMedicine(index)}>
-                            {item.charAt(0).toUpperCase() + item.slice(1)}
+                            {_.startCase(_.toLower(item))}
                         </li>
                     )}
                 </div>
--- src/components/dashboards/sales/medicineInput.jsx
+++ src/components/dashboards/sales/medicineInput.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import axios from 'axios';
-import { toast, ToastContainer } from 'react-toastify';
+import axios from 'axios'
+import { toast, ToastContainer } from 'react-toastify'
+import _ from 'lodash'
 
 import AutoSuggestWrapper from '../../misc/autoSuggestWrapper'
 import config from '../../../config.json'
@@ -8,9 +9,11 @@ import config from '../../../config.json'
 class MedicinesInput extends Component {
     state = { 
         //holds all distinct medicines in {name: medicine} format
-        medicines: [],
+        medicineNames: [],
         //holds current input form value
         value: '',
+        //holds errors if value is not present in medicines
+        errors: {}
     }
 
     async componentDidMount() {
@@ -19,12 +22,12 @@ class MedicinesInput extends Component {
                 method: 'get',
                 url: config.apiEndpoint + "/pharma/get_unique_medicines",
             })
-            var medicines = []
+            var medicineNames = []
             data.forEach((item) => {
                 var obj = { name: item }
-                medicines.push(obj)
+                medicineNames.push(obj)
             })
-            this.setState({ medicines })
+            this.setState({ medicineNames })
         } catch(ex) {
             if(ex.response.status === 400 && ex.response.text)
                 toast.error(ex.response.text)
@@ -37,16 +40,31 @@ class MedicinesInput extends Component {
         this.setState({ value })
     }
 
+    onClick = () => {
+        var errors = {...this.state.errors}
+        const {medicineNames, value} = this.state
+        if(medicineNames.some(elem => elem.name === value)) {
+            delete errors.message
+            this.props.onClick(value)
+        } else {
+            errors.message = _.startCase(_.toLower(value)) + ' doesn\'t exist in our directory.'
+        }
+        this.setState({errors})
+    }
+
     render() { 
-        const { medicines } = this.state;
+        const { medicineNames, errors } = this.state;
         return (  
-            <div className="input-group mb-3">
-                <ToastContainer autoClose={5000}/>
-                <AutoSuggestWrapper items={medicines} placeholder='Enter Medicine...' onChange={this.onChange} />
-                <div className="input-group-append">
-                    <button className="btn btn-outline-secondary" onClick={() => this.props.onClick(this.state.value)} type="button">Forecast</button>
+            <React.Fragment>
+                <div className="input-group mb-3">
+                    <ToastContainer autoClose={5000}/>
+                    <AutoSuggestWrapper items={medicineNames} placeholder='Enter Medicine...' onChange={this.onChange} />
+                    <div className="input-group-append">
+                        <button className="btn btn-outline-secondary" onClick={this.onClick} type="button">Forecast</button>
+                    </div>
                 </div>
-            </div>
+                {errors.message && <div className="alert alert-warning">{errors.message}</div>}
+            </React.Fragment>
         );
     }
 }
