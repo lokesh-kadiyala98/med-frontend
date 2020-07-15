da54561687e60757e7a9727e1d563e9e2e311e4b
lokesh-kadiyala98 <lokesh.pandu1998@gmail.com>
2020-07-15 20:41:38 +0530

    COVID IN map fixed

--- src/components/misc/indiaMap.jsx
+++ src/components/misc/indiaMap.jsx
@@ -6,50 +6,54 @@ import config from '../../config.json'
 
 const IndiaMap = () => {
 
-    async function getStateWiseCoronaData() {
-        const { data } = await axios.get(config.apiEndpoint + '/corona_stats/get_state_wise_data')
-        return data.items
+    async function getCovidINData() {
+        const { data } = await axios.get(config.apiEndpoint + '/corona_stats/IN')
+        return data
     }
 
     GoogleCharts.load('visualization', '1', {'packages': ['geochart']});
     GoogleCharts.load(drawVisualization);
     
     async function drawVisualization() {
-        const SWCD = await getStateWiseCoronaData()
-
+        const covidINData = await getCovidINData()
+        
         var data = GoogleCharts.api.visualization.arrayToDataTable([
-            ['State Code', 'State', 'Cases', 'Recovered'],
-            ['IN-AN', 'Andaman and Nicobar Islands', SWCD[0].cases, SWCD[0].cured],
-            ['IN-AP', 'Andhra Pradesh', SWCD[1].cases + SWCD[26].cases, SWCD[1].cured + SWCD[26].cured],
-            ['IN-AR', 'Arunachal Pradesh', SWCD[2].cases, SWCD[2].cured],
-            ['IN-AS', 'Assam', SWCD[3].cases, SWCD[3].cured],
-            ['IN-BR', 'Bihar', SWCD[4].cases, SWCD[4].cured],
-            ['IN-CH', 'Chandigarh', SWCD[5].cases, SWCD[5].cured],
-            ['IN-CT', 'Chhattisgarh', SWCD[6].cases, SWCD[6].cured],
-            ['IN-DL', 'Delhi', SWCD[7].cases, SWCD[7].cured],
-            ['IN-GA', 'Goa', SWCD[8].cases, SWCD[8].cured],
-            ['IN-GJ', 'Gujarath', SWCD[9].cases, SWCD[9].cured],
-            ['IN-HR', 'Haryana', SWCD[10].cases, SWCD[10].cured],
-            ['IN-HP', 'Himachal Pradesh', SWCD[11].cases, SWCD[11].cured],
-            ['IN-JK', 'Jammu & Kashmir', SWCD[12].cases, SWCD[12].cured],
-            ['IN-JH', 'Jharkhand', SWCD[13].cases, SWCD[13].cured],
-            ['IN-KA', 'Karnataka', SWCD[14].cases, SWCD[14].cured],
-            ['IN-KL', 'Kerala', SWCD[15].cases, SWCD[15].cured],
-            ['IN-LA', 'Ladakh', SWCD[16].cases, SWCD[16].cured],
-            ['IN-MP', 'Madhya Pradesh', SWCD[17].cases, SWCD[17].cured],
-            ['IN-MH', 'Maharashtra', SWCD[18].cases, SWCD[18].cured],
-            ['IN-MN', 'Manipur', SWCD[19].cases, SWCD[19].cured],
-            ['IN-MZ', 'Mizoram', SWCD[20].cases, SWCD[20].cured],
-            ['IN-OR', 'Odisha', SWCD[21].cases, SWCD[21].cured],
-            ['IN-PY', 'Puducherry', SWCD[22].cases, SWCD[22].cured],
-            ['IN-PB', 'Punjab', SWCD[23].cases, SWCD[23].cured],
-            ['IN-RJ', 'Rajasthan', SWCD[24].cases, SWCD[24].cured],
-            ['IN-TN', 'Tamil Nadu', SWCD[25].cases, SWCD[25].cured],
-            // ['IN-TG', 'Telangana', SWCD[26].cases, SWCD[26].cured],
-            ['IN-TR', 'Tripura', SWCD[27].cases, SWCD[27].cured],
-            ['IN-UP', 'Uttar Pradesh',  SWCD[28].cases, SWCD[28].cured],
-            ['IN-UT', 'Uttarakhand', SWCD[29].cases, SWCD[29].cured],
-            ['IN-WB', 'West Bengal', SWCD[30].cases, SWCD[30].cured], 
+            ['State Code', 'State', 'Current', 'Recovered'],
+            ['IN-AN', 'Andaman and Nicobar Islands', covidINData[0].cases, covidINData[0].cured],
+            ['IN-AP', 'Andhra Pradesh', covidINData[1].cases + covidINData[26].cases, covidINData[1].cured + covidINData[26].cured],
+            ['IN-AR', 'Arunachal Pradesh', covidINData[2].cases, covidINData[2].cured],
+            ['IN-AS', 'Assam', covidINData[3].cases, covidINData[3].cured],
+            ['IN-BR', 'Bihar', covidINData[4].cases, covidINData[4].cured],
+            ['IN-CH', 'Chandigarh', covidINData[5].cases, covidINData[5].cured],
+            ['IN-CT', 'Chhattisgarh', covidINData[6].cases, covidINData[6].cured],
+            ['IN-DN', 'Dadra and Nagar Haveli', covidINData[7].cases, covidINData[7].cured],
+            ['IN-DL', 'Delhi', covidINData[8].cases, covidINData[8].cured],
+            ['IN-GA', 'Goa', covidINData[9].cases, covidINData[9].cured],
+            ['IN-GJ', 'Gujarath', covidINData[10].cases, covidINData[10].cured],
+            ['IN-HR', 'Haryana', covidINData[11].cases, covidINData[11].cured],
+            ['IN-HP', 'Himachal Pradesh', covidINData[12].cases, covidINData[12].cured],
+            ['IN-JK', 'Jammu & Kashmir', covidINData[13].cases, covidINData[13].cured],
+            ['IN-JH', 'Jharkhand', covidINData[14].cases, covidINData[14].cured],
+            ['IN-KA', 'Karnataka', covidINData[15].cases, covidINData[15].cured],
+            ['IN-KL', 'Kerala', covidINData[16].cases, covidINData[16].cured],
+            ['IN-LA', 'Ladakh', covidINData[17].cases, covidINData[17].cured],
+            ['IN-MP', 'Madhya Pradesh', covidINData[18].cases, covidINData[18].cured],
+            ['IN-MH', 'Maharashtra', covidINData[19].cases, covidINData[19].cured],
+            ['IN-MN', 'Manipur', covidINData[20].cases, covidINData[20].cured],
+            ['IN-ML', 'Meghalaya', covidINData[21].cases, covidINData[21].cured],
+            ['IN-MZ', 'Mizoram', covidINData[22].cases, covidINData[22].cured],
+            ['IN-NL', 'Nagaland', covidINData[23].cases, covidINData[23].cured],
+            ['IN-OR', 'Odisha', covidINData[24].cases, covidINData[24].cured],
+            ['IN-PY', 'Puducherry', covidINData[25].cases, covidINData[25].cured],
+            ['IN-PB', 'Punjab', covidINData[26].cases, covidINData[26].cured],
+            ['IN-RJ', 'Rajasthan', covidINData[27].cases, covidINData[27].cured],
+            ['IN-SK', 'Sikkim', covidINData[28].cases, covidINData[28].cured],
+            ['IN-TN', 'Tamil Nadu', covidINData[29].cases, covidINData[29].cured],
+            // ['IN-TG', 'Telangana', covidINData[30].cases, covidINData[30].cured],
+            ['IN-TR', 'Tripura', covidINData[31].cases, covidINData[31].cured],
+            ['IN-UP', 'Uttar Pradesh',  covidINData[32].cases, covidINData[32].cured],
+            ['IN-UT', 'Uttarakhand', covidINData[33].cases, covidINData[33].cured],
+            ['IN-WB', 'West Bengal', covidINData[34].cases, covidINData[34].cured], 
         ]);
         
         var opts = {
